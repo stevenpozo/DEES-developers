d8f21fbc53a607d6797e0ca34960dfd2cbf6c7c8
stevenpozo <sjpozo1@espe.edu.ec>
2022-07-25 17:50:41 -0500

    Code

---
+++ 06-Code/SEGUNDO PARCIAL/CODE INTERFACE_PROJECT/LeatherFlowerSystem V.2.0.1/Leather Flower/src/ec/edu/espe/Spa/controller/CustomerInterface.java
@@ -0,0 +1,14 @@
+
+package ec.edu.espe.Spa.controller;
+
+import org.bson.Document;
+
+
+/**
+ *
+ * @author Steven Pozo, Erick Moreira, Elkin Pabón, Diego Ponce, DCCO-ESPE, DEES Developers
+ */
+public interface CustomerInterface {
+public Document RegisterAndBuildDocument(String fullNames,String surnames, String identification, String cellphone, String email,String typeOfService, String cost, String timeOfTreatment, String date, String hourOfAttention);
+ 
+}
